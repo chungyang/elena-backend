6d2697ca944236a507bf5ad321fdb9ea6596ae05
chung <chung.yang@engineer.uconn.edu>
2020-03-25 22:48:28 -0400

    allow fine grained cors

--- src/main/java/com/elena/elena/controller/RouteController.java
+++ src/main/java/com/elena/elena/controller/RouteController.java
@@ -2,10 +2,7 @@ package com.elena.elena.controller;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * Created by chungyang on 3/25/20.
@@ -14,14 +11,15 @@ import org.springframework.web.bind.annotation.RestController;
 public class RouteController {
 
     @RequestMapping(method= RequestMethod.GET, value="/{from}/{to}")
-    public ResponseEntity<String> getRouteCoordinates(@PathVariable String from, @PathVariable String to){
+    @CrossOrigin("http://localhost:3000")
+    public String getRouteCoordinates(@PathVariable String from, @PathVariable String to){
 
         System.out.println(from);
         System.out.println(to);
-        String body = "{values: [ [42.704202, -71.502017], [42.7036844, -71.5020453] ," +
-                "[42.7035846, -71.5020392] ok]}";
+        String body = "{\"values\": [ [42.704202, -71.502017], [42.7036844, -71.5020453] ," +
+                "[42.7035846, -71.5020392]]}";
 
-        return new ResponseEntity<String>(body, HttpStatus.OK);
+        return body;
     }
 
 }
