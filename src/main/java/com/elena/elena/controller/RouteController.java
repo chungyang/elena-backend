41affffb1c687bbcb6f87a0f79acfcca18171da4
chung <chung.yang@engineer.uconn.edu>
2020-03-28 23:14:36 -0400

    change REST endpoint

--- src/main/java/com/elena/elena/controller/RouteController.java
+++ src/main/java/com/elena/elena/controller/RouteController.java
@@ -10,14 +10,18 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 public class RouteController {
 
-    @RequestMapping(method= RequestMethod.GET, value="/{from}/{to}")
+    @RequestMapping(method= RequestMethod.GET, value="search")
     @CrossOrigin("http://localhost:3000")
     @ResponseBody
-    public ResponseEntity<String> getRouteCoordinates(@PathVariable String from, @PathVariable String to){
+    public ResponseEntity<String> getRouteCoordinates(@RequestParam("from") String from,
+                                                      @RequestParam("to") String to,
+                                                      @RequestParam("algorithm") String algorithm,
+                                                      @RequestParam("elemode") String elevationMode,
+                                                      @RequestParam("percentage") int percentage){
 
         String body = "{\"values\": [ [42.704202, -71.502017], [42.7036844, -71.5020453] ," +
                 "[42.7035846, -71.5020392]]}";
-        ResponseEntity<String> responseEntity = new ResponseEntity<String>(body, HttpStatus.BAD_REQUEST);
+        ResponseEntity<String> responseEntity = new ResponseEntity<String>(body, HttpStatus.OK);
 
 
         return responseEntity;
